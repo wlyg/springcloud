85b898ed5976b57758fbcf9e7b4cb6f08cd28faf
sean <13700324600@163.com>
2019-06-04 22:46:18 +0800

    feat(booting): add eureka #1

---
+++ booting-eureka-consumer/src/main/java/com/booting/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.booting.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.booting.pojo.User;
+import com.booting.service.UserService;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping("/consumer")
+	public List<User> getUsers(){
+		return this.userService.getUsers();
+	}
+}
---
+++ booting-eureka-server/src/main/java/com/booting/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.booting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}

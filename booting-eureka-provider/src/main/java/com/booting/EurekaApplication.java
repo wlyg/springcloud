0ac4e2de017922582def49f56754fe5099756c1e
sean <13700324600@163.com>
2019-06-12 21:58:28 +0800

    feat(booting):add hystrix #7

--- booting-eureka-provider/src/main/java/com/booting/EurekaApplication.java
+++ booting-eureka-provider/src/main/java/com/booting/EurekaApplication.java
@@ -2,8 +2,11 @@ package com.booting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 @SpringBootApplication
 public class EurekaApplication {
--- booting-eureka-provider/src/main/java/com/booting/controller/UserController.java
+++ booting-eureka-provider/src/main/java/com/booting/controller/UserController.java
@@ -7,16 +7,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.booting.pojo.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class UserController {
 
+	@HystrixCommand(fallbackMethod = "testFallBack")
 	@RequestMapping("/user")
 	public List<User> getUsers(){
 		List list = new ArrayList<>();
 		list.add("user1");
 		list.add("user2");
 		list.add("user3");
+		int i = 1 / 0;
+
+		return list;
+	}
+
+	public List<User> testFallBack () {
+		List list = new ArrayList<>();
+		list.add("1");
+		list.add("2");
+		list.add("3");
 
 		return list;
 	}
